3befbd1a52967f0293dee219ea4d0982782c4146
Zhanybekk <zanybek03@gmail.com>
2021-09-04 00:50:20 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$('#slider').slick(
+    {
+        arrows:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    }
+)
